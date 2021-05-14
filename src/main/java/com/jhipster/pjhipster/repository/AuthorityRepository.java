2b6988c4d6023927296a497279d95d1361a0e2e2
DanielE2051 <carlos_estrada932012@outlook.com>
2021-05-14 04:36:28 -0500

    repositorionuevo

---
+++ src/main/java/com/jhipster/pjhipster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.jhipster.pjhipster.repository;
+
+import com.jhipster.pjhipster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
